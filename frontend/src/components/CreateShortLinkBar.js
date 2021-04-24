e872d7f88b6cd2a9a61fc5d66f8d6a1ec7f072ff
mariosobhy <mariosobhy0123@outlook.com>
2021-04-24 03:28:01 +0200

    Add some changes

--- frontend/src/components/CreateShortLinkBar.js
+++ frontend/src/components/CreateShortLinkBar.js
@@ -1,71 +1,78 @@
 import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { CreateShortUrl } from "../service/CreateShortUrl";
-import { Button, Grid, TextField, FormControl } from "@material-ui/core";
+import { Button, Grid, TextField } from "@material-ui/core";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
     margin: 50,
     direction: "row",
     justify: "center",
-    alignItems: "center"
+    alignItems: "center",
   },
 }));
 
 export default function SearchBar() {
-  const [url, setUrl] = useState('');
-  const [slug, setSlug] = useState('');
-  const [shortUrl, setShortUrl] = useState('');
+  const [url, setUrl] = useState("");
+  const [slug, setSlug] = useState("");
+  const [shortUrl, setShortUrl] = useState("");
   const classes = useStyles();
 
   const submit = async (e) => {
     e.preventDefault();
-    let params = {url: url, slug: slug};
-    console.log(params)
+    let params = { url: url, slug: slug };
+    console.log(params);
     let response = await CreateShortUrl(params);
     console.log(params);
-    console.log(response.data.data)
-    if(response.data){
-        setShortUrl(response.data.data.shorten_url)
+    console.log(response.data.data);
+    if (response.data) {
+      setShortUrl(response.data.data.shorten_url);
     }
-  }
+  };
   const handleUrlChange = async (e) => {
     setUrl(e.target.value);
-  }
+  };
 
-  const handleSlugChange= async (e) => {
+  const handleSlugChange = async (e) => {
     setSlug(e.target.value);
-  }
+  };
 
   return (
     <>
       <div className={classes.root}>
-        <Grid container spacing={3}>
-        <FormControl xs={12} sm={12} fullWidth={true}>
-          <Grid>
+        <Grid>
+          <form onSubmit={submit}>
+            <Grid>
+              <TextField
+                value={url}
+                onChange={handleUrlChange}
+                label="Enter a long URL"
+                margin="normal"
+                variant="outlined"
+                fullWidth={true}
+                required={true}
+              />
+            </Grid>
+            <Grid>
             <TextField
-            value={url}
-            onChange={handleUrlChange}
-            label="Enter a long URL"
-            margin="normal"
-            variant="outlined"
-            fullWidth={true}
-            required={true}
-            />
-          </Grid>
-            <TextField
-            value={slug}
-            onChange={handleSlugChange}
-            label="Type slug here"
-            margin="normal"
-            variant="outlined"
-            />
+                value={slug}
+                onChange={handleSlugChange}
+                label="Type slug here"
+                margin="normal"
+                variant="outlined"
+              />
+            </Grid>
+            <Grid>
+              <Button variant="contained" color="primary" type="submit">
+                Create
+              </Button>
+            </Grid>
+          </form><br />
           <Grid>
-            <Button variant="contained" color="primary" onClick={submit}>Create</Button>
-          </Grid>
-        </FormControl>
-          <Grid item xs={12} sm={4}>
-            <a href={url} target="_blank">{shortUrl}</a>
+            <p>Short URL: </p>
+            <a href={url} target="_blank">
+              {shortUrl}
+            </a>
           </Grid>
         </Grid>
       </div>
