bf0e33a3645e3f642f44796118402e6d75abd261
mariosobhy <mariosobhy0123@outlook.com>
2021-04-24 14:51:22 +0200

    Add some requests testing

--- api/spec/requests/links_spec.rb
+++ api/spec/requests/links_spec.rb
@@ -18,46 +18,47 @@ RSpec.describe "/links", type: :request do
     {"CONTENT_TYPE" => "application/json"}
   }
 
-#   describe "GET /show" do
-#     it "renders a successful response" do
-#       link = Link.create! valid_attributes
-#       get link_url(link), as: :json
-#       expect(response).to be_successful
-#     end
-#   end
+  # describe "GET /s/:slug" do
+  #   it "renders a successful response" do
+  #     get "s/test",
+  #     params: valid_attributes, headers: valid_headers, as: :json
+  #     link = Link.create! valid_attributes
+  #     get link_url(link), as: :json
+  #     expect(response).to be_successful
+  #   end
+  # end
 
   describe "POST /create" do
     context "with valid parameters" do
       it "creates a short Link" do
-        puts valid_attributes
         expect {
           post "#{links_url}/create",
                params: valid_attributes, headers: valid_headers, as: :json
         }.to change(Link, :count).by(1)
       end
 
-    #   it "renders a JSON response with the new link" do
-    #     post links_url,
-    #          params: { link: valid_attributes }, headers: valid_headers, as: :json
-    #     expect(response).to have_http_status(:created)
-    #     expect(response.content_type).to match(a_string_including("application/json"))
-    #   end
+      it "renders a JSON response with the new link" do
+        post "#{links_url}/create",
+             params: valid_attributes, headers: valid_headers, as: :json
+        expect(response).to have_http_status(:created)
+        expect(response.content_type).to match(a_string_including("application/json"))
+      end
     end
 
     context "with invalid parameters" do
-      # it "does not create a short Link" do
-      #   expect {
-      #     post "#{links_url}/create",
-      #     params: {:url=>"jamison", :slug=>""}, headers: valid_headers, as: :json
-      #   }.to change(Link, :count).by(0)
-      # end
+      it "does not create a short Link" do
+        expect {
+          post "#{links_url}/create",
+          params: invalid_attributes, headers: valid_headers, as: :json
+        }.to change(Link, :count).by(0)
+      end
 
-      # it "renders a JSON response with errors for the new link" do
-      #   post links_url,
-      #        params: { link: invalid_attributes }, headers: valid_headers, as: :json
-      #   expect(response).to have_http_status(:unprocessable_entity)
-      #   expect(response.content_type).to eq("application/json")
-      # end
+      it "renders a JSON response with errors for the new link" do
+        post "#{links_url}/create",
+             params: invalid_attributes, headers: valid_headers, as: :json
+        expect(response).to have_http_status(:unprocessable_entity)
+        expect(response.content_type).to match(a_string_including("application/json"))
+      end
     end
   end
 end
